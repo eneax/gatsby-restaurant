3e886ff9bb1b6e69193381a2d7aa3db9b8b44451
eneax <19818937+eneax@users.noreply.github.com>
2019-02-09 09:59:25 +0100

    Styled NavbarHeader component

--- src/components/globals/navbar/NavbarHeader.js
+++ src/components/globals/navbar/NavbarHeader.js
@@ -1,11 +1,45 @@
 import React, { Component } from 'react'
+import { Link } from 'gatsby'
+import logo from '../../../images/logo.svg'
+import { FaAlignRight } from 'react-icons/fa'
+import styled from 'styled-components'
+
 
 export default class NavbarHeader extends Component {
   render() {
+    const { handleNavbar } = this.props
+
     return (
-      <div>
-        <p>NavbarHeader</p>
-      </div>
+      <HeaderWrapper>
+        <Link to='/'>
+          <img src={logo} alt="Company name"/>
+        </Link>
+        <FaAlignRight 
+          className='toggle-icon'
+          onClick={() => {
+            handleNavbar()
+          }}
+        />
+      </HeaderWrapper>
     )
   }
 }
+
+const HeaderWrapper = styled.div`
+  padding: 0.4rem 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  .toggle-icon {
+    font-size: 1.75rem;
+    color: yellowgreen;
+    cursor: pointer;
+  }
+
+  @media (min-width: 768px) {
+    .toggle-icon {
+      display: none;
+    }
+  }
+`
