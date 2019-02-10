eddbbed0d945fa56ebd1489a801f0d1087475412
eneax <19818937+eneax@users.noreply.github.com>
2019-02-10 11:19:29 +0100

    NavbarLinks setup

--- src/components/globals/navbar/NavbarLinks.js
+++ src/components/globals/navbar/NavbarLinks.js
@@ -1,11 +1,56 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
+import { Link } from 'gatsby'
+import { styles } from '../../../utils'
+
 
 export default class NavbarLinks extends Component {
+  state = {
+    links: [
+      {
+        id: 0,
+        path: '/',
+        name: 'home'
+      },
+      {
+        id: 1,
+        path: '/about/',
+        name: 'about',
+      },
+      {
+        id: 2,
+        path: '/menu/',
+        name: 'menu',
+      },
+      {
+        id: 3,
+        path: '/contact/',
+        name: 'contact'
+      },
+    ]
+  }
+
   render() {
     return (
-      <div>
-        <p>NavbarLinks</p>
-      </div>
+      <LinkWrapper open={this.props.navbarOpen}>
+        {
+          this.state.links.map((item) => (
+            <li key={item.id}>
+              <Link 
+                to={item.path}
+                className="nav-link"
+              >
+                {item.name}
+              </Link>
+            </li>
+          ))
+        }
+      </LinkWrapper>
     )
   }
 }
+
+
+const LinkWrapper = styled.ul`
+
+`
