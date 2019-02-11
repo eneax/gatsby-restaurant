2986e06c41c5ae5db7c4338665cc60c5df53a3f6
eneax <19818937+eneax@users.noreply.github.com>
2019-02-11 10:58:13 +0100

    Add PageHeader component to menu page

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,12 +1,17 @@
 import React from "react"
+
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { PageHeader } from '../utils'
+import menuImg from '../images/bcg/menuBcg.jpeg'
 
 
 const Menu = () => (
   <Layout>
     <SEO title="Menu" keywords={[`gatsby`, `application`, `react`]} />
-    <h3>Menu</h3>
+    <PageHeader img={menuImg}>
+      Menu
+    </PageHeader>
   </Layout>
 )
 
