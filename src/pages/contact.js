e6ab789750bc340d63a1d30e5d6f3f0797fd895e
eneax <19818937+eneax@users.noreply.github.com>
2019-02-11 11:28:14 +0100

    Add Banner component to contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,7 +2,7 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { PageHeader } from '../utils'
+import { PageHeader, Banner } from '../utils'
 import contactImg from '../images/bcg/contactBcg.jpeg'
 
 
@@ -10,7 +10,7 @@ const Contact = () => (
   <Layout>
     <SEO title="Contact" keywords={[`gatsby`, `application`, `react`]} />
     <PageHeader img={contactImg}>
-      Contact
+      <Banner title='contact us' subtitle={`let's get in touch`} />
     </PageHeader>
   </Layout>
 )
