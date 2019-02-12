bc99bac8d50f28666a9ca019d925fb41a63bb3e3
eneax <19818937+eneax@users.noreply.github.com>
2019-02-12 09:56:29 +0100

    Request multiple images

--- src/components/HomePageComponents/Gallery.js
+++ src/components/HomePageComponents/Gallery.js
@@ -6,21 +6,66 @@ import Img from "gatsby-image"
 import { styles, Section } from '../../utils'
 
 
+const SINGLE_IMAGE = graphql`
+  {
+    img1: file(relativePath: { eq: "homeGallery/img-1.jpeg" }) {
+      childImageSharp {
+        fluid(maxWidth:500) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    img2: file(relativePath: { eq: "homeGallery/img-2.jpeg" }) {
+      childImageSharp {
+        fluid(maxWidth:500) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    img3: file(relativePath: { eq: "homeGallery/img-3.jpeg" }) {
+      childImageSharp {
+        fluid(maxWidth:500) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
+
+
 export default function Gallery() {
   return (
     <StaticQuery 
-      query={graphql`
-        {
-          img1: file(relativePath: { eq: "homeGallery/img-1.jpeg" }) {
-            childImageSharp {
-              fluid(maxWidth:500) {
-                ...GatsbyImageSharpFluid
-              }
-            }
-          }
-        }
-      `}
-      render={data => <Img fluid={data.img1.childImageSharp.fluid} />}
+      query={SINGLE_IMAGE}
+      render={data => {
+        const img1 = data.img1.childImageSharp.fluid;
+        const img2 = data.img2.childImageSharp.fluid;
+        const img3 = data.img3.childImageSharp.fluid;
+
+        return (
+          <Section>
+            <GalleryWrapper>
+              <div className="item item-1">
+                <Img fluid={img1} />
+                <p className="info">awesome pizza</p>
+              </div>
+              <div className="item item-2">
+                <Img fluid={img2} />
+                <p className="info">awesome pizza</p>
+              </div>
+              <div className="item item-3">
+                <Img fluid={img3} />
+                <p className="info">awesome pizza</p>
+              </div>
+            </GalleryWrapper>
+          </Section>
+        )
+      }}
     />
   )
 }
+
+
+const GalleryWrapper = styled.div`
+
+`
