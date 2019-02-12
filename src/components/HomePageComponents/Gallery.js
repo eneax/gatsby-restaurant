45c7012259ac76065338ba23ee5400ec57fbeacb
eneax <19818937+eneax@users.noreply.github.com>
2019-02-12 10:15:26 +0100

    Style gallery section with CSS Grid

--- src/components/HomePageComponents/Gallery.js
+++ src/components/HomePageComponents/Gallery.js
@@ -51,11 +51,11 @@ export default function Gallery() {
               </div>
               <div className="item item-2">
                 <Img fluid={img2} />
-                <p className="info">awesome pizza</p>
+                <p className="info">awesome pork</p>
               </div>
               <div className="item item-3">
                 <Img fluid={img3} />
-                <p className="info">awesome pizza</p>
+                <p className="info">awesome steak</p>
               </div>
             </GalleryWrapper>
           </Section>
@@ -67,5 +67,46 @@ export default function Gallery() {
 
 
 const GalleryWrapper = styled.div`
+  display: grid;
+  grid-template-columns: auto;  /* span the column as much as possible --> 1col layout */
+  grid-row-gap: 1rem;           /* space between the rows */
 
+  .item {
+    position: relative;
+  }
+
+  .info {
+    position: absolute;
+    top: 0;
+    left: 0;
+    background: ${styles.colors.mainYellow};
+    padding: .1rem .3rem;
+    text-transform: capitalize;
+  }
+
+  @media (min-width: 576px) {
+    grid-template-columns: 1fr 1fr;  /* 2 cols of equal width */
+    grid-column-gap: 1rem;
+  }
+  @media (min-width: 768px) {
+    grid-template-columns: repeat(3, 1fr);  /* 1fr 1fr 1fr */
+  }
+  @media (min-width: 992px) {
+    .gatsby-image-wrapper {
+      height: 100%;
+    }
+
+    grid-template-areas: 
+    'one one two two'
+    'one one three three';
+    .item-1 {
+      grid-area: one;
+    }
+    .item-2 {
+      grid-area: two;
+    }
+    .item-3 {
+      grid-area: three;
+    }
+  }
 `
