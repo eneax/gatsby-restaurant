a163f2613376ad7e773b7ec840b1840e274ce71f
eneax <19818937+eneax@users.noreply.github.com>
2019-02-13 10:32:06 +0100

    Refactor GlobalStyle

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,29 +1,15 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { createGlobalStyle } from "styled-components"
 
+import GlobalStyle from './styles/GlobalStyle'
 import Navbar from "../components/globals/navbar"
 import Footer from "../components/globals/Footer"
 
 
-const GLobalStyle = createGlobalStyle`
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-
-  body {
-    font-family: 'Open Sans', sans-serif;
-    color: #262626;
-    background: #fff;
-  }
-`
-
 const Layout =  ({ children }) => {
   return (
     <>
-      <GLobalStyle />
+      <GlobalStyle />
       <Navbar />
       {children}
       <Footer />
---
+++ src/components/styles/GlobalStyle.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components"
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: 'Open Sans', sans-serif;
+    color: #262626;
+    background: #fff;
+  }
+`
+
+export default GlobalStyle
