dddadb025c91d5f6d4c447fec763de9a2a0b9801
eneax <19818937+eneax@users.noreply.github.com>
2019-02-14 11:11:58 +0100

    Test netlify forms

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -13,6 +13,21 @@ const Contact = () => (
     <PageHeader img={contactImg}>
       <Banner title='contact us' subtitle={`let's get in touch`} />
     </PageHeader>
+
+    <form name="contact" method="POST" data-netlify="true">
+      <p>
+        <label>Your Name: <input type="text" name="name" /></label>
+      </p>
+      <p>
+        <label>Your Email: <input type="email" name="email" /></label>
+      </p>
+      <p>
+        <label>Message: <textarea name="message"></textarea></label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
   </Layout>
 )
 
