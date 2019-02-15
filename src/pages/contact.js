172dad19b818c08255acd08a64dafd405752c441
eneax <19818937+eneax@users.noreply.github.com>
2019-02-15 10:10:48 +0100

    Add info to contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,36 +3,34 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Banner from "../components/banner"
+import Title from '../components/title'
+import Form from '../components/form'
+import Section from '../components/styles/section'
 import { PageHeader } from '../components/styles/headers'
+import { BannerButton } from '../components/styles/buttons'
 import contactImg from '../images/bcg/contactBcg.jpeg'
 
+import { FaPhone } from 'react-icons/fa'
 
 const Contact = () => (
   <Layout>
     <SEO title="Contact" keywords={[`gatsby`, `application`, `react`]} />
     <PageHeader img={contactImg}>
-      <Banner title='contact us' subtitle={`let's get in touch`} />
+      <Banner title='contact us' subtitle={`let's get in touch`}>
+        <a href="tel:+18506484200">
+          <BannerButton style={{ margin: '2rem auto' }}>
+            <FaPhone /> 850-648-4200
+          </BannerButton>
+        </a>
+      </Banner>
     </PageHeader>
 
-    <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
-      <input type="hidden" name="bot-field" />
-      <div className="field half first">
-        <label htmlFor="name">Name</label>
-        <input type="text" name="name" id="name" />
-      </div>
-      <div className="field half">
-        <label htmlFor="email">Email</label>
-        <input type="text" name="email" id="email" />
-      </div>
-      <div className="field">
-        <label htmlFor="message">Message</label>
-        <textarea name="message" id="message" rows="6"></textarea>
-      </div>
-      <ul className="actions">
-        <li><input type="submit" value="Send Message" className="special" /></li>
-        <li><input type="reset" value="Clear" /></li>
-      </ul>
-    </form>
+    <Section>
+      <Title title='11AM-2AM' message='open everyday' />
+
+      <Form />
+    </Section>
+
   </Layout>
 )
 
