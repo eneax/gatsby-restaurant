3275b2e7175ff1c25d56e0187386b36ef4ca9768
eneax <19818937+eneax@users.noreply.github.com>
2019-02-16 14:27:01 +0100

    Update favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,13 +22,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Delicious`,
+        short_name: `Delicious`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#FFBB00`,
+        theme_color: `#FFBB00`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/logo.svg`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
