d1d56aa3f6703cdbd0d973833e1853ea58a46e9f
eneax <19818937+eneax@users.noreply.github.com>
2019-02-16 14:17:40 +0100

    Style 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,12 +2,17 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { HomeHeader } from '../components/styles/headers'
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <HomeHeader>
+      <h1>
+        NOT FOUND 
+        <span role='img' aria-label='disappointed emoji'> 😥</span>
+      </h1> 
+    </HomeHeader>
   </Layout>
 )
 
